7c9722f80d0978aa591bcbb10787c6ac35d89753
Papai-Paul <papaipaul9641@gmail.com>
2026-05-17 23:45:56 +0530

    Add navbar

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require('ejs-mate');
 
 main()
     .then(() => {
@@ -24,12 +25,16 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method")); 
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(port, () => {
     console.log(`server is listening to port ${port}`);
 });  
 
-
+app.get ("/", (req, res) => {
+    res.send ("Hi I am Root");
+});
 
 app.get("/listings", async (req, res) => {
     const allListings = await Listing.find({});
